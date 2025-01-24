12919d74d3e40b325254f408598e1d1d9c096217
Amrito Karmokar <amritokarmokar5i@gmail.com>
2025-01-24 17:17:46 +0000

    Revert "Fix build for Bluetooth App"
    This reverts commit 40ae09ea1002a3954003b9f32a70d4773f521c72.

--- android/app/src/com/android/bluetooth/btservice/AdapterService.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -1101,7 +1101,7 @@ public class AdapterService extends Service {
         sendBroadcastMultiplePermissions(switchBufferSizeIntent, new String[] {
                 android.Manifest.permission.BLUETOOTH_CONNECT,
                 android.Manifest.permission.BLUETOOTH_PRIVILEGED
-        }, (String[]) null);
+        }, null);
     }
 
     void switchCodecCallback(boolean isLowLatencyBufferSize) {
@@ -4393,18 +4393,6 @@ public class AdapterService extends Service {
         @Override
         public boolean isBroadcastActive(AttributionSource attributionSource) {
             return false;
-        }
-
-        @Override
-        public void getEncKeyMaterialValue(SynchronousResultReceiver receiver) {
-            try {
-                receiver.send(getEncKeyMaterialValue());
-            } catch (RuntimeException e) {
-                receiver.propagateException(e);
-            }
-        }
-        private byte[] getEncKeyMaterialValue() {
-            return null;
         }
 
         @Override
